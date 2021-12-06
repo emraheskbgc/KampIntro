b3e35781566f422fa4e2c5375939062df59cafe5
emrah eskibağcı <emraheskibagci595@gmail.com>
2021-12-07 00:29:07 +0300

    07.12.2021 Ders Sonu

---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //class çağırma
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.ProductName = "Masa";
+            //2-Mboilya
+            product1.CategoryId = 2;
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 3;
+            //bu şekilde de oluşturulur 
+            Product product2 = new Product { ID = 2, CategoryId = 5, UnitInStock = 5, ProductName="Kalem", UnitPrice=35 };
+                
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+        
+        }
+    }
+}
